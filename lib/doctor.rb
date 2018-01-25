47fe4cef121fcd5f2b975fc720b5613cf1232495
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-25 20:22:01 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,8 +6,8 @@ class Doctor
     @appointments = []
   end
 
-  def add_appointment(i)
-    @appointments << i
+  def add_appointment(appointment)
+    @appointments << appointment
   end
 
   def appointments
