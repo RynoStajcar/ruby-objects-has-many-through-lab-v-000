e0ac02e41f1dc09aa1c9a2754e30320f7ad3fa43
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-25 20:55:40 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,6 +15,6 @@ class Doctor
   end
 
   def patients
-    self.add_appointment.collect {|appointment| appointment}
+    self.add_appointment.collect {|appointment| appointment.patient}
   end
 end
